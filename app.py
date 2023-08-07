ddcdb01675244d77b6966baf2f00c189679b4fd5
Shiva <98306948+ShivaBollam07@users.noreply.github.com>
2023-08-07 11:26:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from tensorflow.keras.preprocessing import image
 from keras.models import load_model
 from PIL import Image
 
-# Load the trained CNN model
 cnn = load_model('trained_model.h5')  # Load the saved model
 
-# Function to predict the uploaded image
 def predict_image(image_path):
     test_image = image.load_img(image_path, target_size=(64, 64))
     test_image = image.img_to_array(test_image)
@@ -21,26 +19,21 @@ def predict_image(image_path):
     
     return prediction
 
-# Streamlit app
 st.title("Pneumonia Classifier")
 st.write("Upload an X-ray image to check for pneumonia.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Display the uploaded image
     st.subheader("Uploaded Image:")
     st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
     
-    # Save the uploaded image locally
     image_path = './uploaded_image.jpg'
     with open(image_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     
-    # Get the prediction
     prediction = predict_image(image_path)
     
-    # Display the prediction
     st.subheader("Prediction:")
     if prediction == 'Pneumonia Detected':
         st.error(prediction)
